a6c5d68b3eb8efd4356271f34ab1ccc8059424ab
Tien <tienht0511@gmail.com>
2026-03-18 07:11:45 +0700

    test branch nè

---
+++ test_branch.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("Chào mừng bạn đến với WSL!\n");
+    printf("Đây là chương trình C đầu tiên của bạn trên WSL.\n");
+    printf("Chúc bạn học tập hiệu quả và vui vẻ với lập trình C trên WSL!\n");
+    return 0;
