f787d44e294e899bee01d467d47ae88691d5b8da
Tien <tienht0511@gmail.com>
2026-03-18 10:24:33 +0700

    Thêm thông điệp chào mừng vào chương trình C

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main() {
     printf("Chào mừng bạn đến với WSL!\n");
     printf("Đây là chương trình C đầu tiên của bạn trên WSL.\n");
+    printf("Hãy tận hưởng việc lập trình C trên môi trường Linux!\n");
     return 0;
 
 }
