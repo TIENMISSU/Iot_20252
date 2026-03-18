c96837ca32caf005ea37d494a19ef41313df9e73
Tien <tienht0511@gmail.com>
2026-03-18 10:25:20 +0700

    cập nhật code

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main() {
     printf("Chào mừng bạn đến với WSL!\n");
     printf("Đây là chương trình C đầu tiên của bạn trên WSL.\n");
     printf("Hãy tận hưởng việc lập trình C trên môi trường Linux!\n");
+    printf("echo 'Xin chào, WSL!' > hello.txt\n");
     return 0;
 
-}
+}   
